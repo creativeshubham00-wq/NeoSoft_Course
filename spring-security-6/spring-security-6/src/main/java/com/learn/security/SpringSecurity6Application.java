bc20d3bfcaa01cd2e5579eb353314df8281e45a4
Shubham Gupta <creativeshubham00@gmail.com>
2026-01-12 16:11:55 +0530

    changes in spring-security

--- spring-security-6/spring-security-6/src/main/java/com/learn/security/SpringSecurity6Application.java
+++ spring-security-6/spring-security-6/src/main/java/com/learn/security/SpringSecurity6Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurity6Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurity6Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity6Application.class, args);
+    }
 }
